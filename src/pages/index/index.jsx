de760ca5f0f85bcf9c4c8f028a357ccdbf1776b8
Khondamir <xondamir.maxmudov.01@mail.ru>
2023-10-10 17:06:40 +0500

    fixed

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -38,7 +38,7 @@ const Index = () => {
                         animate={{opacity: 1, translateX: 0}}
                         transition={{delay: 0.3}}
                     >
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 
                     </motion.p>
                 </div>
