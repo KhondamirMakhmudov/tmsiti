ab44d6a6a1d77342df9cd83730beb862d58687f7
Khondamir <xondamir.maxmudov.01@mail.ru>
2023-10-27 07:48:46 +0500

    news:pagination

--- src/components/pagination/index.jsx
+++ src/components/pagination/index.jsx
@@ -1,27 +1,53 @@
-import React from 'react';
-import ReactPaginate from 'react-paginate';
+import React from "react";
+import ReactPaginate from "react-paginate";
 import Image from "next/image";
 
 const Pagination = ({
-                        page = 1,
-                        pageCount = 10,
-                        setPage = () => {
-                        },
-                        className
-
-                    }) => {
-    return (
-        <div className={className}>
-            <ReactPaginate forcePage={page - 1} onPageChange={({selected}) => setPage(selected + 2)}
-                           pageCount={pageCount}
-                           nextLabel={<span className={'bg-black'}><Image src={'/icons/arrow-right.svg'} alt={'right'} width={24} height={24} className={'bg-[#1A4DC2] rounded-[5px] p-[3px]'}/></span>}
-                           previousLabel={<span><Image src={'/icons/arrow-left.svg'} alt={'left'} width={24} height={24} className={'bg-[#1A4DC2] rounded-[5px] p-[3px]'}/></span>} className={'flex justify-center my-2 items-center'}
-                           pageClassName={'ml-2.5'}
-                           pageLinkClassName={'inline-flex min-w-[32px] h-8 rounded-[5px] justify-center items-center bg-transparent text-[#001A57] text-lg p-1'}
-                           nextClassName={'ml-2.5  text-sm text-[#313B3D]'} previousClassName={'text-sm text-[#313B3D]'}
-                           breakLinkClassName={'ml-4 mr-1.5'} activeLinkClassName={'!bg-[#1A4DC2] !text-white'}/>
-        </div>
-    );
+  page = 1,
+  pageCount = 5,
+  setPage = () => {},
+  className,
+}) => {
+  return (
+    <div className={className}>
+      <ReactPaginate
+        forcePage={page - 1}
+        onPageChange={({ selected }) => setPage(selected + 1)}
+        pageCount={pageCount}
+        nextLabel={
+          <span className={"bg-black"}>
+            <Image
+              src={"/icons/arrow-right.svg"}
+              alt={"right"}
+              width={24}
+              height={24}
+              className={"bg-[#1A4DC2] rounded-[5px] p-[3px]"}
+            />
+          </span>
+        }
+        previousLabel={
+          <span>
+            <Image
+              src={"/icons/arrow-left.svg"}
+              alt={"left"}
+              width={24}
+              height={24}
+              className={"bg-[#1A4DC2] rounded-[5px] p-[3px]"}
+            />
+          </span>
+        }
+        className={"flex justify-center my-2 items-center"}
+        pageClassName={"ml-2.5"}
+        pageLinkClassName={
+          "inline-flex min-w-[32px] h-8 rounded-[5px] justify-center items-center bg-transparent text-[#001A57] text-lg p-1"
+        }
+        nextClassName={"ml-2.5  text-sm text-[#313B3D]"}
+        previousClassName={"text-sm text-[#313B3D]"}
+        breakLinkClassName={"ml-4 mr-1.5"}
+        activeLinkClassName={"!bg-[#1A4DC2] !text-white"}
+      />
+    </div>
+  );
 };
 
 export default Pagination;
--- src/pages/news/index.jsx
+++ src/pages/news/index.jsx
@@ -2,61 +2,71 @@ import React from "react";
 import Main from "@/layouts/main";
 import Menu from "@/components/menu";
 import Title from "@/components/title";
-import {KEYS} from "@/constants/key";
-import {URLS} from "@/constants/url";
-import {get} from "lodash";
+import { KEYS } from "@/constants/key";
+import { URLS } from "@/constants/url";
+import { get } from "lodash";
 import NewsTemplate from "@/components/news-template";
 import useGetTMSITIQuery from "@/hooks/api/useGetTMSITIQuery";
-import {useRouter} from "next/router";
-import {motion} from "framer-motion";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 import SkeletonLoader from "@/components/loader/skeleton";
-
+import NewsList from "@/containers/news-list";
 
 const News = () => {
+  const {
+    data: news,
+    isLoading: isLoadingNews,
+    isFetching: isFetchingNews,
+  } = useGetTMSITIQuery({ key: KEYS.news, url: URLS.news });
 
+  return (
+    <Main>
+      <Menu active={0} />
 
-    const {data: news, isLoading:isLoadingNews, isFetching: isFetchingNews} = useGetTMSITIQuery({key: KEYS.news,
-        url: URLS.news,
-    })
+      <div
+        className={
+          "grid grid-cols-12 gap-x-[30px] container mx-auto px-[20px] md:px-0"
+        }
+      >
+        <motion.div
+          initial={{ translateX: "-200px" }}
+          animate={{ translateX: "0px" }}
+          className={"col-span-12 mt-[50px] mb-[30px]"}
+        >
+          <Title>Barcha yangiliklar</Title>
+        </motion.div>
 
+        {/*<ul className={'col-span-12'}>*/}
+        {/*    {*/}
+        {/*        get(news, 'data.results', []).map(newsItem =>*/}
+        {/*            <li key={get(newsItem, 'id')}>*/}
+        {/*                {isFetchingNews && isLoadingNews ? <SkeletonLoader/> :*/}
+        {/*                    <NewsTemplate*/}
+        {/*                        imgUrl={(get(newsItem, 'news_image'))}*/}
+        {/*                        dateTime={get(newsItem, 'news_datetime')}*/}
+        {/*                        title={get(newsItem, 'news_title')}*/}
+        {/*                        description={get(newsItem, 'news_desc')}*/}
+        {/*                        url={`/news/${get(newsItem,'id','#')}`}*/}
+        {/*                    />*/}
+        {/*                }*/}
 
+        {/*            </li>*/}
+        {/*        )*/}
+        {/*    }*/}
 
-    return (
-        <Main>
-            <Menu active={0}/>
-
-            <div className={'grid grid-cols-12 gap-x-[30px] container mx-auto px-[20px] md:px-0'}>
-                <motion.div initial={{translateX: '-200px'}} animate={{translateX: '0px'}}  className={'col-span-12 mt-[50px] mb-[30px]'}>
-                    <Title>
-                        Barcha yangiliklar
-                    </Title>
-                </motion.div>
-
-                <ul className={'col-span-12'}>
-                    {
-                        get(news, 'data.results', []).map(newsItem =>
-                            <li key={get(newsItem, 'id')}>
-                                {isFetchingNews && isLoadingNews ? <SkeletonLoader/> :
-                                    <NewsTemplate
-                                        imgUrl={(get(newsItem, 'news_image'))}
-                                        dateTime={get(newsItem, 'news_datetime')}
-                                        title={get(newsItem, 'news_title')}
-                                        description={get(newsItem, 'news_desc')}
-                                        url={`/news/${get(newsItem,'id','#')}`}
-                                    />
-                                }
-
-                            </li>
-                        )
-                    }
-
-                </ul>
-
-            </div>
-
-
-        </Main>
-    );
+        {/*</ul>*/}
+        <div className={"col-span-12"}>
+          <NewsList
+            key={KEYS.news}
+            url={URLS.news}
+            news_title={"news_title"}
+            news_description={"news_desc"}
+            imgUrl={"news_image"}
+          />
+        </div>
+      </div>
+    </Main>
+  );
 };
 
 export default News;
