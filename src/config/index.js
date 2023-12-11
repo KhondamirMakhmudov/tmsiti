8e8ceb49c4f497ad029fee5f3d4a8bb291fe9864
Khondamir <xondamir.maxmudov.01@mail.ru>
2023-12-11 17:59:10 +0500

    fixed

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const config = {
-    BASE_SHNK_URL: 'https://shnk.rcsc.uz/api/v1/',
-    BASE_TMSITI_URL: 'https://tmsiti.rcsc.uz/api/v1',
-    FILE_URL: 'https://shnk.rcsc.uz',
-    DEFAULT_APP_LANG:'en'
+  BASE_SHNK_URL: "https://shnk.rcsc.uz/api/v1/",
+  BASE_TMSITI_URL: "https://backend.tmsiti.uz/api/v1/",
+  FILE_URL: "https://shnk.rcsc.uz",
+  DEFAULT_APP_LANG: "en",
 };
