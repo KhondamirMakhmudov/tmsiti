35730fde46efd1d6ec9b3eef0ac97ca7694b9fcf
Khondamir <xondamir.maxmudov.01@mail.ru>
2023-12-20 10:11:49 +0500

    fixed:_document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,54 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-        <Html lang="en">
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" crossOrigin="true"/>
-            </Head>
-              <body className={'overflow-x-hidden'}>
-                <Main />
-                <NextScript />
-              </body>
-        </Html>
-  )
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+          crossOrigin="true"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+          crossOrigin="true"
+        />
+      </Head>
+      <body className={"overflow-x-hidden"}>
+        <Main />
+        <NextScript />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(12345678, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });
+              `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/12345678"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+      </body>
+    </Html>
+  );
 }
