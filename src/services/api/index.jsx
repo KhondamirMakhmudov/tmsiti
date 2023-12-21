b83cf6b54436396052af06d53bdf6cba2d1101c7
Khondamir <xondamir.maxmudov.01@mail.ru>
2023-12-21 17:46:08 +0500

    shnk replace api

--- src/services/api/index.jsx
+++ src/services/api/index.jsx
@@ -1,70 +1,69 @@
-import axios from 'axios';
-import {config} from "@/config";
-import storage from "@/services/storage";
-import {get} from 'lodash';
-
-
-// Request for SHNK
-const requestSHNK = axios.create({
-    baseURL: config.BASE_SHNK_URL,
-    params: {},
-    headers: {
-        common: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-    },
-})
-
-requestSHNK.interceptors.request.use((config) => {
-    const token = get(JSON.parse(storage.get('settings')), 'state.token', null);
-    if (token) {
-        config.headers['token'] = `${token}`
-    }
-
-    return config;
-}, (error) => {
-    return  Promise.reject(error);
-    }
-);
-
-requestSHNK.interceptors.response.use((response) => {
-    return response;
-}, (error) => {
-    return Promise.reject(error);
-})
-
-
-// Request for TMSITI
-const requestTMSITI = axios.create({
-    baseURL: config.BASE_TMSITI_URL,
-    params: {},
-    headers: {
-        common: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-    },
-})
-
-
-requestTMSITI.interceptors.request.use((config) => {
-        const token = get(JSON.parse(storage.get('settings')), 'state.token', null);
-        if (token) {
-            config.headers['token'] = `${token}`
-        }
-
-        return config;
-    }, (error) => {
-        return  Promise.reject(error);
-    }
-);
-
-requestTMSITI.interceptors.response.use((response) => {
-    return response;
-}, (error) => {
-    return Promise.reject(error);
-})
-
-export {requestSHNK, requestTMSITI};
-
+// import axios from 'axios';
+// import {config} from "@/config";
+// import storage from "@/services/storage";
+// import {get} from 'lodash';
+//
+//
+// // Request for SHNK
+// const requestSHNK = axios.create({
+//     baseURL: config.BASE_SHNK_URL,
+//     params: {},
+//     headers: {
+//         common: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json; charset=utf-8',
+//         },
+//     },
+// })
+//
+// requestSHNK.interceptors.request.use((config) => {
+//     const token = get(JSON.parse(storage.get('settings')), 'state.token', null);
+//     if (token) {
+//         config.headers['token'] = `${token}`
+//     }
+//
+//     return config;
+// }, (error) => {
+//     return  Promise.reject(error);
+//     }
+// );
+//
+// requestSHNK.interceptors.response.use((response) => {
+//     return response;
+// }, (error) => {
+//     return Promise.reject(error);
+// })
+//
+//
+// // Request for TMSITI
+// const requestTMSITI = axios.create({
+//     baseURL: config.BASE_TMSITI_URL,
+//     params: {},
+//     headers: {
+//         common: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json; charset=utf-8',
+//         },
+//     },
+// })
+//
+//
+// requestTMSITI.interceptors.request.use((config) => {
+//         const token = get(JSON.parse(storage.get('settings')), 'state.token', null);
+//         if (token) {
+//             config.headers['token'] = `${token}`
+//         }
+//
+//         return config;
+//     }, (error) => {
+//         return  Promise.reject(error);
+//     }
+// );
+//
+// requestTMSITI.interceptors.response.use((response) => {
+//     return response;
+// }, (error) => {
+//     return Promise.reject(error);
+// })
+//
+// export {requestSHNK, requestTMSITI};
--- src/services/i18n/index.js
+++ src/services/i18n/index.js
@@ -1,40 +1,40 @@
-// import i18n from "i18next";
-// import Backend from "i18next-http-backend";
-// import { initReactI18next } from "react-i18next";
-// import LanguageDetector from "i18next-browser-languagedetector";
-// import { config } from "@/config";
-// import storage from "../storage";
-// import i18next from "i18next";
-//
-// i18n
-//
-//   // Enables the i18next backend
-//   .use(Backend)
-//
-//   // Enable automatic language detection
-//   .use(LanguageDetector)
-//
-//   // Enables the hook initialization module
-//   .use(i18next)
-//   .init({
-//     lng: storage.get("i18nextLng") || config.DEFAULT_APP_LANG,
-//     fallbackLng: storage.get("i18nextLng") || config.DEFAULT_APP_LANG,
-//     backend: {
-//       /* translation file path */
-//       loadPath: `${config.BASE_TMSITI_URL}translations/?lang={{lng}}`,
-//       addPath: `${config.BASE_TMSITI_URL}translations/`,
-//     },
-//     debug: false,
-//     keySeparator: false,
-//     saveMissing: true,
-//     react: {
-//       useSuspense: false,
-//       wait: true,
-//     },
-//     interpolation: {
-//       escapeValue: false,
-//       formatSeparator: ",",
-//     },
-//   });
-//
-// export default i18n;
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { config } from "@/config";
+import storage from "../storage";
+import i18next from "i18next";
+
+i18n
+
+  // Enables the i18next backend
+  .use(Backend)
+
+  // Enable automatic language detection
+  .use(LanguageDetector)
+
+  // Enables the hook initialization module
+  .use(initReactI18next)
+  .init({
+    lng: storage.get("i18nextLng") || config.DEFAULT_APP_LANG,
+    fallbackLng: storage.get("i18nextLng") || config.DEFAULT_APP_LANG,
+    backend: {
+      /* translation file path */
+      loadPath: `${config.BASE_TMSITI_URL}news/?lang={{lng}}`,
+      addPath: `${config.BASE_TMSITI_URL}translations/`,
+    },
+    debug: false,
+    keySeparator: false,
+    saveMissing: true,
+    react: {
+      useSuspense: false,
+      wait: true,
+    },
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ",",
+    },
+  });
+
+export default i18n;
