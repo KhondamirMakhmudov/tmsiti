c5e8e46604a5976595981d01fb4775b8c9fc5431
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-07-15 17:40:24 +0500

    fixed

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -121,7 +121,7 @@ export const menuData = [
       {
         id: 3,
         title: "lab",
-        url: "/laboratory",
+        url: "https://sites.google.com/view/kompleks-sinov-laboratoriyasi/%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F-%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0",
       },
     ],
   },
